6e194ee17e63ac315b71b737966a1a91d56468ec
Willy Jacquet <willy.jacquet@outlook.com>
2021-07-31 13:00:49 +0200

    Vector product by scalar.

--- src/agl/glsl/function/vector/multiplication.hpp
+++ src/agl/glsl/function/vector/multiplication.hpp
@@ -18,4 +18,27 @@ Vec<T, N>& operator*=(Vec<T, N>& lv, Vec<T, N> rv) {
     return lv = lv * rv;
 }
 
+template<typename T, std::size_t N> constexpr
+Vec<T, N> operator*(float lf, Vec<T, N> rv) {
+    auto r = Vec<T, N>();
+    for(std::size_t i = 0; i < N; ++i) {
+        r[i] = lf * rv[i];
+    }
+    return r;
+}
+
+template<typename T, std::size_t N> constexpr
+Vec<T, N> operator*(Vec<T, N>& lv, float rf) {
+    auto r = Vec<T, N>();
+    for(std::size_t i = 0; i < N; ++i) {
+        r[i] = lv[i] * rf;
+    }
+    return r;
+}
+
+template<typename T, std::size_t N> constexpr
+Vec<T, N>& operator*=(Vec<T, N>& lv, float rf) {
+    return lv = lv * rf;
+}
+
 }
