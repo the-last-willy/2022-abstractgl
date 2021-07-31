9005ba6c302bea8c1e1cac0cdb2e6cd562e0cb2d
Willy Jacquet <willy.jacquet@outlook.com>
2021-07-31 13:01:07 +0200

    Get active uniform.

---
+++ src/agl/opengl/function/shader_program/get_active_uniform.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "agl/opengl/function/shader_program/get_program.hpp"
+#include "agl/opengl/qualifier/program.hpp"
+#include "agl/opengl/qualifier/uniform_index.hpp"
+
+#include <string>
+
+namespace agl {
+
+inline
+void get_active_uniform(
+    Program p,
+  	UniformIndex<GLuint> ui,
+  	GLsizei bufSize,
+  	GLsizei *length,
+  	GLint *size,
+  	GLenum *type,
+  	GLchar *name)
+{
+	glGetActiveUniform(
+        p,
+        ui,
+        bufSize,
+        length,
+        size,
+        type,
+        name);
+}
+
+struct UniformInformation {
+    std::string name;
+    GLint size;
+    GLenum type;
+};
+
+inline 
+UniformInformation active_uniform(
+    Program p,
+  	UniformIndex<GLuint> ui)
+{
+    auto i = UniformInformation();
+    i.name.resize(active_uniform_max_length(p));
+    auto length = GLsizei(0);
+    get_active_uniform(
+        p,
+        ui,
+        static_cast<GLsizei>(std::size(i.name)),
+        &length,
+        &i.size,
+        &i.type,
+        data(i.name));
+    i.name.resize(length);
+    return i;
+}
+
+}
