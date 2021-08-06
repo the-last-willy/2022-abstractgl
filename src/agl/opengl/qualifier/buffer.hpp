214aa83ce610826f54c375e9b7e6c963f394cca0
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-06 14:04:51 +0200

    Implicit conversion from buffer to bool.

--- src/agl/opengl/qualifier/buffer.hpp
+++ src/agl/opengl/qualifier/buffer.hpp
@@ -6,6 +6,11 @@ namespace agl {
 
 struct Buffer : Qualifier<GLuint> {
     using Qualifier<GLuint>::Qualifier;
+
+    constexpr
+    operator bool() const noexcept {
+        return value;
+    }
 };
 
 }
