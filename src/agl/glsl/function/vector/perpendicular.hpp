4f4cf2b5dbd7e08837507f5329e4c9589038574e
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-12 12:32:16 +0200

    Less incorrect vector perpendicular fn.

--- src/agl/glsl/function/vector/perpendicular.hpp
+++ src/agl/glsl/function/vector/perpendicular.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
+#include "agl/glsl/function/geometric/cross.hpp"
 #include "agl/glsl/vec/vec3.hpp"
 
 namespace agl {
 
 constexpr
-auto perpendicular(agl::Vec3 v) {
-    return agl::vec3(
-        v[1] - v[2],
-        v[2] - v[0],
-        v[0] - v[1]);
+auto perpendicular(Vec3 v) {
+    // return vec3(
+    //     v[1] - v[2],
+    //     v[2] - v[0],
+    //     v[0] - v[1]);
+    if(v[0] == 1.f) {
+        return cross(v, vec3(0.f, 1.f, 0.f));
+    } else {
+        return cross(v, vec3(1.f, 0.f, 0.f));
+    }
 }
 
 }
