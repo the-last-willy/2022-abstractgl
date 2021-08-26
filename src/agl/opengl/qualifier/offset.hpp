2d75622d2ccd61f973b0f139275bdce66f1a66f6
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-26 14:23:09 +0200

    Factory functions.

--- src/agl/opengl/qualifier/offset.hpp
+++ src/agl/opengl/qualifier/offset.hpp
@@ -9,4 +9,9 @@ struct Offset : Qualifier<Type> {
     using Qualifier<Type>::Qualifier;
 };
 
+template<typename R, typename P = R> constexpr
+Offset<R> offset(P p) {
+    return Offset<R>(static_cast<R>(p));
+}
+
 }
--- src/agl/opengl/qualifier/stride.hpp
+++ src/agl/opengl/qualifier/stride.hpp
@@ -9,4 +9,10 @@ struct Stride : Qualifier<Type> {
     using Qualifier<Type>::Qualifier;
 };
 
+template<typename R, typename P = R> constexpr
+Stride<R> stride(P p) {
+    return Stride<R>(static_cast<R>(p));
+}
+
+
 }
