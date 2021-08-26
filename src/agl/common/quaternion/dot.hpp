613cd2d77a81388c27062dc7352d38ae26c85cec
Willy Jacquet <willy.jacquet@outlook.com>
2021-08-26 18:18:01 +0200

    Fixed quaternion dot product.

--- src/agl/common/quaternion/dot.hpp
+++ src/agl/common/quaternion/dot.hpp
@@ -12,9 +12,7 @@ namespace agl {
 
 template<typename T> constexpr
 auto dot(const Quaternion<T>& lq, const Quaternion<T>& rq) {
-    auto numerator = lq.real * rq.real + dot(lq.pure, rq.pure);
-    auto denominator = std::sqrt(squared_norm(lq) * squared_norm(rq));
-    return numerator / denominator;
+    return lq.real * rq.real + dot(lq.pure, rq.pure);
 }
 
 }
