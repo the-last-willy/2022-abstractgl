876c362c2d0cd8a2a9b8dffbe4ccf4e3d7ebc200
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-11 18:45:36 +0200

    [glsl] Sum of vec with scalar.

--- src/agl/glsl/function/vector/addition.hpp
+++ src/agl/glsl/function/vector/addition.hpp
@@ -5,6 +5,15 @@
 namespace agl {
 
 template<typename T, std::size_t N> constexpr
+Vec<T, N> operator+(Vec<T, N> lv, float f) {
+    auto r = Vec<T, N>();
+    for(std::size_t i = 0; i < N; ++i) {
+        r[i] = lv[i] + f;
+    }
+    return r;
+}
+
+template<typename T, std::size_t N> constexpr
 Vec<T, N> operator+(Vec<T, N> lv, Vec<T, N> rv) {
     auto r = Vec<T, N>();
     for(std::size_t i = 0; i < N; ++i) {
