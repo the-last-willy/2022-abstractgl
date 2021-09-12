aa17b378dbcac19be713c1a712cb4bdecda32f35
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-12 17:30:17 +0200

    [format/off] Handled faces with more than 3 vertices.

--- src/agl/format/off/all.hpp
+++ src/agl/format/off/all.hpp
@@ -23,13 +23,17 @@ auto read(const std::string& filepath) {
     auto stream = std::stringstream(
         agl::standard::string(std::filesystem::path(filepath)));
 
+    if(stream.peek() == 'O') {
+        stream.ignore(4, '\n');
+    }
+
     std::size_t vertex_count;
     stream >> vertex_count;
     f.vertex_positions.resize(vertex_count);
 
     std::size_t face_count;
     stream >> face_count;
-    f.triangle_indices.resize(face_count);
+    f.triangle_indices.reserve(face_count);
     
     std::size_t edge_count;
     stream >> edge_count;
@@ -39,10 +43,20 @@ auto read(const std::string& filepath) {
         stream >> vp[0] >> vp[1] >> vp[2];
     }
 
+    auto face_indices = std::vector<uint32_t>();
     for(std::size_t i = 0; i < face_count; ++i) {
-        auto& ti = f.triangle_indices[i];
         GLuint face_vertex_count;
-        stream >> face_vertex_count >> ti[0] >> ti[1] >> ti[2];
+        stream >> face_vertex_count;
+        if(face_vertex_count >= size(face_indices)) {
+            face_indices.resize(face_vertex_count);
+        }
+        for(GLuint j = 0; j < face_vertex_count; ++j) {
+            stream >> face_indices[j];
+        }
+        for(GLuint j = 2; j < face_vertex_count; ++j) {
+            f.triangle_indices.emplace_back(agl::Uvec3{
+                face_indices[0], face_indices[j - 1], face_indices[j]});
+        }
     }
 
     return f;
