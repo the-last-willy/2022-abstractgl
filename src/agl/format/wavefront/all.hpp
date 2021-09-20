afb49f17433d53f505e1356847fbbf4a4afc44ed
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-20 23:42:12 +0200

    [format/wavefront] Textures ?

--- src/agl/format/wavefront/all.hpp
+++ src/agl/format/wavefront/all.hpp
@@ -2,6 +2,8 @@
 
 #include <agl/engine/all.hpp>
 
+#include <stb_image.h>
+#include <stb_image_resize.h>
 #include <tiny_obj_loader.h>
 
 #include <filesystem>
@@ -10,6 +12,60 @@
 #include <stdexcept>
 
 namespace agl::format::wavefront {
+
+inline
+auto load_texture(std::string filepath, int factor) {
+
+    int w, h, n;
+    unsigned char *data = stbi_load(filepath.c_str(), &w, &h, &n, 0);
+    if(data == NULL) {
+        throw std::runtime_error("stb_image: Failure.");
+    }
+
+    auto dw = w / factor;
+    auto dh = w / factor;
+    auto downsampled = std::vector<unsigned char>(n * dw * dh);
+
+    auto r = stbir_resize_uint8(
+        data, w, h, 0,
+        downsampled.data(), dw, dh, 0,
+        n);
+    if(r == 0) {
+        throw std::runtime_error("stb_image_resize: Failure.");
+    }
+
+    stbi_image_free(data);
+
+    auto t = agl::create(agl::TextureTarget::_2d);
+    GLenum base_internal_format = 0;
+    GLenum pixel_data_type = 0;
+    GLenum sized_internal_format = 0;
+    if(n == 3) {
+        base_internal_format = GL_RGB;
+        pixel_data_type = GL_UNSIGNED_BYTE;
+        sized_internal_format = GL_RGB8;
+    } else if(n == 4) {
+        base_internal_format = GL_RGBA;
+        pixel_data_type = GL_UNSIGNED_BYTE;
+        sized_internal_format = GL_RGBA8;
+    } else {
+        throw std::runtime_error("Invalid texture format.");
+    }
+    auto level_count = static_cast<GLsizei>(
+        std::floor(std::log2(std::max(dh, dw))) + 1);
+    agl::storage(
+            t, level_count, sized_internal_format,
+            agl::Width(dw), agl::Height(dh));
+    agl::image(
+        t, agl::Width(dw), agl::Height(dh),
+        base_internal_format, pixel_data_type,
+        std::as_bytes(std::span(downsampled)));
+    agl::generate_mipmap(t);
+
+    auto et = eng::Texture();
+    et.texture = t;
+    return et;
+}
 
 struct Content {
     std::vector<std::shared_ptr<eng::Material>> materials;
@@ -55,6 +111,9 @@ Content load(
         auto& material = materials[i];
         auto& eng_material = *(content.materials[i]
             = std::make_shared<eng::Material>());
+        eng_material.textures["baseColorTexture"]
+        = std::make_shared<eng::Texture>(
+            load_texture((mtl_path.string() + material.diffuse_texname).c_str(), 4));
         eng_material.uniforms["baseColorFactor"]
         = new eng::Uniform<agl::Vec4>(agl::vec4(
             material.ambient[0],
