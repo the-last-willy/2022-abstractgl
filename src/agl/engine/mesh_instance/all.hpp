ad6643df7b837d08e78a2ae4d5eeabd8bd2d0680
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-22 23:09:04 +0200

    [engine] Mesh instance bounding box (the dirty way).

--- src/agl/engine/mesh_instance/all.hpp
+++ src/agl/engine/mesh_instance/all.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "bounding_box.hpp"
 #include "mesh_instance.hpp"
 #include "mesh.hpp"
 #include "primitive.hpp"
--- src/agl/engine/mesh_instance/bounding_box.hpp
+++ src/agl/engine/mesh_instance/bounding_box.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "mesh.hpp"
 #include "mesh_instance.hpp"
 
 #include "agl/engine/bounding_box/bounding_box.hpp"
@@ -12,10 +13,9 @@ inline
 BoundingBox bounding_box(const MeshInstance& mi) {
     auto it = mi.uniforms.find("mvp_transform");
     if(it != end(mi.uniforms)) {
-        if(auto& t = dynamic_cast<const agl::Uniform<agl::Mat4>&>(*it)) {
-            auto& a = mesh(mi).attributes
-            return BoundingBox();
-        }
+        auto& t = dynamic_cast<const eng::Uniform<agl::Mat4>&>(*it->second);
+        auto& m = mesh(mi);
+        return BoundingBox(m.bb_min, m.bb_max, eng::Transform(t.value));
     } else {
         throw std::logic_error("Missing mesh instance transform.");
     }
