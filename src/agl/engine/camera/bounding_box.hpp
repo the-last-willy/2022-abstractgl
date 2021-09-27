b261885cd2ddcacd2548f0d0b5706c050a3d44cf
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-27 21:39:49 +0200

    [engine] Fixed camera bounding box ?

--- src/agl/engine/camera/bounding_box.hpp
+++ src/agl/engine/camera/bounding_box.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#include "agl/engine/bounding_box/bounding_box.hpp"
 #include "agl/engine/camera.hpp"
+#include "view_to_camera_transform.hpp"
+
+#include "agl/engine/bounding_box/bounding_box.hpp"
 
 namespace agl::engine {
 
 inline
 auto bounding_box(const eng::Camera& c) {
-    return BoundingBox(vec3(-1.f), vec3(1.f), transform(c));
+    return BoundingBox(vec3(-1.f), vec3(1.f), view_to_camera_transform(c));
 }
 
 }
