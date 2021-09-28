bc73d79992ab81353bc79d1f603078ed4367c846
Willy Jacquet <willy.jacquet@outlook.com>
2021-09-28 07:44:43 +0200

    [engine] Fixed transform.

--- src/agl/engine/camera/bounding_box.hpp
+++ src/agl/engine/camera/bounding_box.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "agl/engine/camera.hpp"
-#include "view_to_camera_transform.hpp"
+#include "world_to_clip.hpp"
 
 #include "agl/engine/bounding_box/bounding_box.hpp"
 
@@ -9,7 +9,7 @@ namespace agl::engine {
 
 inline
 auto bounding_box(const eng::Camera& c) {
-    return BoundingBox(vec3(-1.f), vec3(1.f), view_to_camera_transform(c));
+    return BoundingBox(vec3(-1.f), vec3(1.f), world_to_clip(c));
 }
 
 }
