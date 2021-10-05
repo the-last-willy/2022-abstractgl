f5a04afbd2232b0d9a418ffa9923dce1b15a4c31
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-05 10:20:51 +0200

    [format/wavefront] Indexed meshes.

--- src/agl/format/wavefront/load_shapes.hpp
+++ src/agl/format/wavefront/load_shapes.hpp
@@ -4,6 +4,8 @@
 
 #include <tiny_obj_loader.h>
 
+#include <fstream>
+#include <map>
 #include <stdexcept>
 
 namespace agl::format::wavefront {
@@ -16,6 +18,7 @@ void load_shapes(
     auto& attrib = obj.GetAttrib();
     auto& shapes = obj.GetShapes();
     content.meshes.resize(size(shapes));
+
     for(std::size_t i = 0; i < size(shapes); ++i) {
         auto& mesh = shapes[i].mesh;
         auto& eng_mesh = *(content.meshes[i] = std::make_shared<eng::Mesh>());
@@ -40,7 +43,9 @@ void load_shapes(
             positions.clear();
             texcoords.clear();
 
-            for(; f < size(mesh.num_face_vertices); ++f) {
+            auto vertex_indices = std::map<std::array<int, 3>, unsigned>();
+
+            for(; f < size(mesh.num_face_vertices); ++f) {                
                 if(mesh.num_face_vertices[f] != 3) {
                     throw std::logic_error(
                         "agl::format::wavefront: "
@@ -49,26 +54,38 @@ void load_shapes(
                 if(mesh.material_ids[f] == material_group) {
                     for(std::size_t j = 0; j < mesh.num_face_vertices[f]; ++j) {
                         auto idx = mesh.indices[3 * f + j];
-                        indices.emplace_back(static_cast<unsigned>(f));
-                        if(idx.normal_index >= 0) {
-                            normals.emplace_back(agl::vec3(
-                                attrib.normals[3 * idx.normal_index + 0],
-                                attrib.normals[3 * idx.normal_index + 1],
-                                attrib.normals[3 * idx.normal_index + 2]));
-                        }
-                        { // Position.
-                            positions.emplace_back(agl::vec3(
-                                attrib.vertices[3 * idx.vertex_index + 0],
-                                attrib.vertices[3 * idx.vertex_index + 1],
-                                attrib.vertices[3 * idx.vertex_index + 2]));
-                        }
-                        if(idx.texcoord_index >= 0) {
-                            texcoords.emplace_back(agl::vec2(
-                                attrib.texcoords[2 * idx.texcoord_index + 0],
-                                attrib.texcoords[2 * idx.texcoord_index + 1]));
+                        auto idx_array = std::array{
+                            idx.vertex_index,
+                            idx.normal_index,
+                            idx.texcoord_index};
+                        auto it = vertex_indices.find(idx_array);
+                        if(it == end(vertex_indices)) {
+                            auto vertex_index = static_cast<unsigned>(size(vertex_indices));
+                            vertex_indices[idx_array] = vertex_index;
+                            indices.emplace_back(vertex_index);
+                            if(idx.normal_index >= 0) {
+                                normals.emplace_back(agl::vec3(
+                                    attrib.normals[3 * idx.normal_index + 0],
+                                    attrib.normals[3 * idx.normal_index + 1],
+                                    attrib.normals[3 * idx.normal_index + 2]));
+                            }
+                            { // Position.
+                                positions.emplace_back(agl::vec3(
+                                    attrib.vertices[3 * idx.vertex_index + 0],
+                                    attrib.vertices[3 * idx.vertex_index + 1],
+                                    attrib.vertices[3 * idx.vertex_index + 2]));
+                            }
+                            if(idx.texcoord_index >= 0) {
+                                texcoords.emplace_back(agl::vec2(
+                                    attrib.texcoords[2 * idx.texcoord_index + 0],
+                                    attrib.texcoords[2 * idx.texcoord_index + 1]));
+                            }
+                        } else {
+                            indices.emplace_back(it->second);
                         }
                     }
                 } else {
+                    vertex_indices.clear();
                     break;
                 }
             }
@@ -84,83 +101,6 @@ void load_shapes(
             eng_primitive.primitive_count =  agl::Count<GLsizei>(
                 static_cast<GLsizei>(3 * size(indices)));
         }
-        
-        // OLD WAY.
-        // { // Load primitive.
-        //     auto& eng_primitive = *(eng_mesh.primitives.emplace_back(
-        //         std::make_shared<eng::Primitive>()));
-        //     if(mesh.material_ids.front() >= 0) {
-        //         eng_primitive.material = content.materials[mesh.material_ids.front()];
-        //     }
-        //     { // Positions.
-        //         auto indices = std::vector<unsigned>(size(mesh.indices));
-        //         auto normals = std::vector<agl::Vec3>(size(mesh.indices));
-        //         auto positions = std::vector<agl::Vec3>(size(mesh.indices));
-        //         auto texcoords = std::vector<agl::Vec2>(size(mesh.indices));
-        //         auto pmin = agl::vec3(
-        //             attrib.vertices[0],
-        //             attrib.vertices[1],
-        //             attrib.vertices[2]);
-        //         auto pmax = pmin;
-        //         for(std::size_t f = 0; f < size(mesh.indices); ++f) {
-        //             auto idx = mesh.indices[f];
-        //             indices[f] = static_cast<unsigned>(f);
-        //             if(idx.normal_index >= 0) {
-        //                 normals[f] = agl::vec3(
-        //                     attrib.normals[3 * idx.normal_index + 0],
-        //                     attrib.normals[3 * idx.normal_index + 1],
-        //                     attrib.normals[3 * idx.normal_index + 2]);
-        //             }
-        //             { // Position.
-        //                 auto p = positions[f] = agl::vec3(
-        //                     attrib.vertices[3 * idx.vertex_index + 0],
-        //                     attrib.vertices[3 * idx.vertex_index + 1],
-        //                     attrib.vertices[3 * idx.vertex_index + 2]);
-        //                 if(p[0] < pmin[0]) {
-        //                     pmin[0] = p[0];
-        //                 } else if(p[0] > pmax[0]) {
-        //                     pmax[0] = p[0];
-        //                 }
-        //                 if(p[1] < pmin[1]) {
-        //                     pmin[1] = p[1];
-        //                 } else if(p[1] > pmax[1]) {
-        //                     pmax[1] = p[1];
-        //                 }
-        //                 if(p[2] < pmin[2]) {
-        //                     pmin[2] = p[2];
-        //                 } else if(p[2] > pmax[2]) {
-        //                     pmax[2] = p[2];
-        //                 }
-        //             }
-        //             if(idx.texcoord_index >= 0) {
-        //                 texcoords[f] = agl::vec2(
-        //                     attrib.texcoords[2 * idx.texcoord_index + 0],
-        //                     attrib.texcoords[2 * idx.texcoord_index + 1]);
-        //             }
-        //         }
-        //         eng_primitive.attributes["vn"] = agl::engine::accessor(
-        //             std::span(normals));
-        //         eng_primitive.attributes["v"] = agl::engine::accessor(
-        //             std::span(positions));
-        //         {
-        //             eng_primitive.attributes["v"].min.resize(3);
-        //             eng_primitive.attributes["v"].min[0] = pmin[0];
-        //             eng_primitive.attributes["v"].min[1] = pmin[1];
-        //             eng_primitive.attributes["v"].min[2] = pmin[2];
-        //             eng_primitive.attributes["v"].max.resize(3);
-        //             eng_primitive.attributes["v"].max[0] = pmax[0];
-        //             eng_primitive.attributes["v"].max[1] = pmax[1];
-        //             eng_primitive.attributes["v"].max[2] = pmax[2];
-        //         }
-        //         eng_primitive.attributes["vt"] = agl::engine::accessor(
-        //             std::span(texcoords));
-        //         eng_primitive.draw_type = agl::DrawType::unsigned_int;
-        //         eng_primitive.indices = agl::engine::accessor(
-        //             std::span(indices));
-        //         eng_primitive.primitive_count =  agl::Count<GLsizei>(
-        //             static_cast<GLsizei>(size(mesh.indices)));
-        //     }
-        // }
     }
     // Compute bounding boxes.
     for(std::size_t s = 0; s < size(shapes); ++s) {
