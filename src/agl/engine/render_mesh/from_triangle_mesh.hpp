0de18822d2cd0aac37ac5487ad51106bc9396b17
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-19 08:00:48 +0200

    [engine] TMesh to render mesh.

--- src/agl/engine/render_mesh/from_triangle_mesh.hpp
+++ src/agl/engine/render_mesh/from_triangle_mesh.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "agl/engine/accessor/all.hpp"
+#include "agl/engine/triangle_mesh/all.hpp"
+#include "agl/engine/material.hpp"
 #include "agl/engine/mesh.hpp"
 
 #include "agl/geometry/all.hpp"
@@ -8,12 +10,23 @@
 namespace agl::engine {
 
 inline
-auto render_mesh(mesh::triangle::Mesh& tm) {
+auto render_mesh(agl::engine::TriangleMesh& tm, std::vector<std::shared_ptr<eng::Material>> materials = {}) {
     auto m = eng::Mesh();
     auto& p = *(m.primitives.emplace_back(std::make_shared<eng::Primitive>()));
+    p.draw_type = agl::DrawType::unsigned_int;
     p.indices = accessor(std::span(tm.indices));
+    p.primitive_count = agl::Count<GLsizei>(
+        static_cast<GLsizei>(size(tm.indices)));
     if(not empty(tm.positions)) {
         p.attributes["v"] = accessor(std::span(tm.positions));
+    }
+    if(not empty(tm.material_ids) && not empty(materials)) {
+        auto mid = tm.material_ids.front();
+        if(mid < size(materials)) {
+            p.material = materials[mid];
+        } else {
+            throw std::logic_error("Missing material.");
+        }
     }
     return m;
 }
