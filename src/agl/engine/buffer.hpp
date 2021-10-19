240f049f21d7b614b20401b0a53e772ea5e8dab4
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-19 21:14:33 +0200

    [engine] Buffer destruction.

--- src/agl/engine/buffer.hpp
+++ src/agl/engine/buffer.hpp
@@ -7,8 +7,18 @@
 namespace eng {
 
 struct Buffer {
+    Buffer() = default;
+
+    Buffer(const Buffer&) = delete;
+
+    Buffer(Buffer&&) = default;
+
+    ~Buffer() {
+        delete_(opengl);
+    }
+
     std::vector<std::byte> data = {};
-    agl::Buffer opengl = {};
+    agl::Buffer opengl;
 };
 
 }
