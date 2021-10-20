14dc69445639b8fd749d356b6ce2a2908969fe2c
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-20 23:37:25 +0200

    [engine] Updated wavefront load geometry for new mesh.

--- src/agl/format/wavefront/load_geometry.hpp
+++ src/agl/format/wavefront/load_geometry.hpp
@@ -27,35 +27,37 @@ agl::engine::TriangleMesh load_geometry(
         // Loop over faces(polygon)
         size_t index_offset = 0;
         for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-            size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+            auto fv = uint32_t(shapes[s].mesh.num_face_vertices[f]);
 
+            auto&& face = create_face(m, fv);
             // Loop over vertices in the face.
             for (size_t v = 0; v < fv; v++) {
-                topology(m).face_indices.push_back(static_cast<uint32_t>(size(topology(m).face_indices)));
+                auto&& vertex = create_vertex(m); // topology(m).face_indices.push_back(static_cast<uint32_t>(size(topology(m).face_indices)));
+                topology(face).incident_vertices[v] = index(vertex);
 
                 // access to vertex
                 tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
 
-                geometry(m).vertex_material_ids.push_back(shapes[s].mesh.material_ids[f]);
+                material_id(vertex) = shapes[s].mesh.material_ids[f];
 
-                geometry(m).vertex_positions.push_back(agl::vec3(
+                position(vertex) = agl::vec3(
                     attrib.vertices[3*size_t(idx.vertex_index)+0],
                     attrib.vertices[3*size_t(idx.vertex_index)+1],
-                    attrib.vertices[3*size_t(idx.vertex_index)+2]));
+                    attrib.vertices[3*size_t(idx.vertex_index)+2]);
 
                 // Check if `normal_index` is zero or positive. negative = no normal data
                 if (idx.normal_index >= 0) {
-                    geometry(m).vertex_normals.push_back(agl::vec3(
+                    normal(vertex) = agl::vec3(
                         attrib.normals[3*size_t(idx.normal_index)+0],
                         attrib.normals[3*size_t(idx.normal_index)+1],
-                        attrib.normals[3*size_t(idx.normal_index)+2]));
+                        attrib.normals[3*size_t(idx.normal_index)+2]);
                 }
 
                 // Check if `texcoord_index` is zero or positive. negative = no texcoord data
                 if (idx.texcoord_index >= 0) {
-                    geometry(m).vertex_texcoords.push_back(agl::vec2(
+                    texcoords(vertex) = agl::vec2(
                         attrib.texcoords[2*size_t(idx.texcoord_index)+0],
-                        attrib.texcoords[2*size_t(idx.texcoord_index)+1]));
+                        attrib.texcoords[2*size_t(idx.texcoord_index)+1]);
                 }
             }
             index_offset += fv;
