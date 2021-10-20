b15a49801a4530f45c4851ff19f6d68b67760be0
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-20 12:32:27 +0200

    [engine] Triangle mesh proper counts.
    Co-authored-by: Yoan Parra <Devskil@users.noreply.github.com>

--- src/agl/engine/triangle_mesh/mesh/all.hpp
+++ src/agl/engine/triangle_mesh/mesh/all.hpp
@@ -6,13 +6,26 @@
 namespace agl::engine {
 
 inline
+const Topology& topology(const TriangleMesh& tm) {
+    return tm.topology;
+}
+
+inline
+Topology& topology(TriangleMesh& tm) {
+    return tm.topology;
+}
+
+inline
 uint32_t vertex_count(const TriangleMesh& tm) {
+    // return vertex_count(topology(tm));
     return static_cast<uint32_t>(size(tm.indices));
 }
 
 inline
 uint32_t face_count(const TriangleMesh& tm) {
+    // return face_count(topology(tm));
     return static_cast<uint32_t>(size(tm.indices) / tm.vertex_per_face);
+
 }
 
 inline
--- src/agl/engine/triangle_mesh/topology/all.hpp
+++ src/agl/engine/triangle_mesh/topology/all.hpp
@@ -5,13 +5,15 @@
 namespace agl::engine {
 
 inline
-uint32_t face_count(const Topology& t) {
-    return t.face_count;
+uint32_t vertex_count(const Topology& t) {
+    // return t.vertex_count;
+    return static_cast<uint32_t>(size(t.face_indices));
 }
 
 inline
-uint32_t vertex_count(const Topology& t) {
-    return t.vertex_count;
+uint32_t face_count(const Topology& t) {
+    // return t.face_count;
+    // return vertex_count(t) / t.vertex_per_face;
 }
 
 }
