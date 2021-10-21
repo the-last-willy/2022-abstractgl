f9b12fbb431b57592b5c7b1eb716ccd67ff1d26f
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-21 20:18:43 +0200

    [engine] Used seemingly working render fn in render pass.

--- src/agl/engine/render_pass/render.hpp
+++ src/agl/engine/render_pass/render.hpp
@@ -26,11 +26,12 @@ void render(const RenderPass& rp) {
             }
             upload_to(*s.mesh, program(rp));
             for(std::size_t i = 0; i < size(s.vertex_arrays); ++i) {
+                // Upload material if there is one.
                 if(primitive(*s.mesh, i).material) {
                     upload_to(*primitive(*s.mesh, i).material, program(rp));
                 }
                 bind(s.vertex_arrays[i]);
-                eng::render(primitive(*s.mesh, i));
+                eng::render(primitive(*s.mesh, i), s.vertex_arrays[i]);
             }
         }
     }
