10015a57d13c9527c2047375bc037052a0a01518
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-21 14:59:48 +0200

    [engine] Re-enabled point mesh.

--- src/agl/engine/render_mesh/point_mesh.hpp
+++ src/agl/engine/render_mesh/point_mesh.hpp
@@ -15,26 +15,26 @@ auto point_mesh(agl::engine::TriangleMesh& tm, std::vector<std::shared_ptr<eng::
     (void) materials;
     auto m = eng::Mesh();
     auto indices = std::vector<uint32_t>();
-    auto positions = accessor(std::span(geometry(tm).vertex_positions));
-    // std::cout << size(geometry(tm).vertex_positions) << std::endl;
-    // for(uint32_t vi = 0; vi < vertex_count(tm);) {
-    //     auto& p = *m.primitives.emplace_back(std::make_shared<eng::Primitive>());
-    //     {
-    //         p.attributes["v"] = positions;
-    //         p.draw_mode = agl::DrawMode::points;
-    //         p.draw_type = agl::DrawType::unsigned_int;
-    //     }
+    auto positions = std::make_shared<eng::Accessor>(
+        accessor(std::span(geometry(tm).vertex_positions)));
+    for(uint32_t vi = 0; vi < vertex_count(tm);) {
+        auto& p = *m.primitives.emplace_back(std::make_shared<eng::Primitive>());
+        {
+            p.attributes["v"] = positions;
+            p.draw_mode = agl::DrawMode::points;
+            p.draw_type = agl::DrawType::unsigned_int;
+        }
 
-    //     for(; vi < vertex_count(tm); ++vi) {
-    //         auto&& v = vertex(tm, vi);
-    //         indices.push_back(index(v));
-    //     }
+        for(; vi < vertex_count(tm); ++vi) {
+            auto&& v = vertex(tm, vi);
+            indices.push_back(index(v));
+        }
 
-    //     p.indices = accessor(std::span(indices));
-    //     p.primitive_count = agl::Count<GLsizei>(
-    //         static_cast<GLsizei>(size(indices)));
-    //     indices.clear();
-    // }
+        p.indices = std::make_shared<eng::Accessor>(accessor(std::span(indices)));
+        p.primitive_count = agl::Count<GLsizei>(
+            static_cast<GLsizei>(size(indices)));
+        indices.clear();
+    }
     return m;
 }
 
