67987675957eea7f38c15d0280d496ff8415024f
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-23 23:33:15 +0200

    [engine] Wrapping incident vertex.

--- src/agl/engine/triangle_mesh/topology/face/incident_vertex.hpp
+++ src/agl/engine/triangle_mesh/topology/face/incident_vertex.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "incident_vertex_count.hpp"
 #include "topology.hpp"
 
 namespace agl::engine {
 
 inline
 uint32_t incident_vertex(const FaceTopology& t, uint32_t i) {
-    return t.incident_vertices[i];
+    return t.incident_vertices[i % incident_vertex_count(t)];
 }
 
 }
