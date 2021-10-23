75898d2ab06f593a8723539cf51cd03514dd6ce5
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-23 23:33:59 +0200

    [engine] Set vertex count inside factory.

--- src/agl/engine/triangle_mesh/topology/mesh/create_face.hpp
+++ src/agl/engine/triangle_mesh/topology/mesh/create_face.hpp
@@ -2,15 +2,16 @@
 
 #include "proxy/vertex/proxy.hpp"
 #include "face_count.hpp"
-#include "mesh.hpp"
+#include "topology.hpp"
 
 #include <span>
 
 namespace agl::engine {
 
 inline
-uint32_t create_face(Topology& t, uint32_t vertex_count) {
-    t.faces.emplace_back(vertex_count);
+uint32_t create_face(MeshTopology& t, uint32_t vertex_count) {
+    auto&& f = t.faces.emplace_back();
+    f.incident_vertex_count = vertex_count;
     return static_cast<uint32_t>(size(t.faces) - 1);
 }
 
