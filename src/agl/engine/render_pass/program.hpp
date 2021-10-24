9e23ce8fee6707af682525148340a8aaa53b52e1
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-24 22:15:09 +0200

    [engine] RenderPass assign program fn.

--- src/agl/engine/render_pass/program.hpp
+++ src/agl/engine/render_pass/program.hpp
@@ -2,11 +2,29 @@
 
 #include "render_pass.hpp"
 
+#include <memory>
+
 namespace agl::engine {
 
 inline
 auto& program(const RenderPass& rp) {
     return *rp.program;
+}
+
+inline
+void assign_program(
+    RenderPass& rp,
+    const std::shared_ptr<eng::Program>& p)
+{
+    rp.program = p;
+}
+
+inline
+auto& assign_program(
+    RenderPass& rp,
+    eng::Program&& p)
+{
+    return rp.program = std::make_shared<eng::Program>(std::move(p));
 }
 
 }
