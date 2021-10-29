cdb7718fb356b8ebb394e5e654834a9d5ce02db5
Willy Jacquet <willy.jacquet@outlook.com>
2021-10-29 23:05:17 +0200

    [glsl] 2D cross product.

--- src/agl/glsl/function/geometric/cross.hpp
+++ src/agl/glsl/function/geometric/cross.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "agl/glsl/vec/vec2.hpp"
 #include "agl/glsl/vec/vec3.hpp"
 
 namespace agl {
+
+constexpr
+float cross(Vec2 lv, Vec2 rv) {
+    return lv[0] * rv[1] - lv[1] * rv[0];
+}
 
 constexpr
 Vec3 cross(Vec3 lv, Vec3 rv) {
