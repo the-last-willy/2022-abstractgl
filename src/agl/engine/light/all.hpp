60ecba934761cfbfa0b03508c2a476298285e71c
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-05 10:38:00 +0100

    [engine] Improted lights (as comments) from another project.

---
+++ src/agl/engine/light/all.hpp
@@ -0,0 +1,17 @@
+// struct DirectionalLight {
+//     agl::Vec3 direction = {};
+//     agl::Mat4 transform = {};
+
+//     std::shared_ptr<eng::Texture> shadow_map = std::make_shared<eng::Texture>();;
+// };
+
+// struct SpotLight {
+//     agl::Vec3 direction = {};
+//     agl::Mat4 transform = {};
+
+//     std::shared_ptr<eng::Texture> shadow_map = std::make_shared<eng::Texture>();;
+// };
+
+// struct PointLight {
+//     agl::Vec3 position = {};
+// };
