d0ae87739e3b8c97f6a518c1a275cf4d3863c585
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-11 21:44:52 +0100

    [glsl] Fixed mix.

--- src/agl/glsl/function/common/mix.hpp
+++ src/agl/glsl/function/common/mix.hpp
@@ -2,6 +2,11 @@
 
 namespace agl {
 
+template<typename T, std::size_t N> constexpr
+auto mix(const Vec<T, N>& lt, const Vec<T, N>& rt, const Vec<T, N>& i) {
+    return (vec<T, N>(1) - i) * lt + i * rt;
+}
+
 template<typename T, typename I> constexpr
 auto mix(const T& lt, const T& rt, const I i) {
     return (I(1) - i) * lt + i * rt;
