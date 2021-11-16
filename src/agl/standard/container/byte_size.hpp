22f52858c2f532a845b1c286c207c47c1facc8a5
Willy Jacquet <willy.jacquet@outlook.com>
2021-11-16 17:45:25 +0100

    [standard/container] Fixed container byte size.

--- src/agl/standard/container/byte_size.hpp
+++ src/agl/standard/container/byte_size.hpp
@@ -4,9 +4,13 @@
 
 namespace agl::standard {
 
-inline
-std::size_t byte_size(const auto& c) {
-    return sizeof(typename decltype(c)::value_type) * size(c);
+template<typename Container>
+requires requires (Container c) {
+    typename Container::value_type;
+    size(c);
+}
+auto byte_size(const Container& c) {
+    return sizeof(typename Container::value_type) * size(c);
 }
 
 }
