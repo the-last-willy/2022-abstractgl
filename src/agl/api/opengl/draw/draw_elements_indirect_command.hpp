d9dd78e1b812aef03fd4f60d20312a7b67489159
Willy Jacquet <willy.jacquet@outlook.com>
2022-01-03 23:23:39 +0100

    [api/opengl] Least surprising default value for draw elements command.

--- src/agl/api/opengl/draw/draw_elements_indirect_command.hpp
+++ src/agl/api/opengl/draw/draw_elements_indirect_command.hpp
@@ -3,8 +3,11 @@
 namespace agl::opengl {
 
 struct DrawElementsIndirectCommand {
+    // Number of indices (not primitives).
     GLuint count = 0;
-    GLuint instanceCount = 0;
+    // '1' seems to be the least surprising value,
+    // when not specified, a single instance is drawn.
+    GLuint instanceCount = 1;
     GLuint firstIndex = 0;
     GLuint baseVertex = 0;
     GLuint baseInstance = 0;
