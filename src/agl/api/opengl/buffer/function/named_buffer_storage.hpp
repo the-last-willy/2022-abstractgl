f64328d6697740ed9284a18ae5c28e30929dcac3
Willy Jacquet <willy.jacquet@outlook.com>
2022-01-07 23:18:24 +0100

    [api/opengl] glNamedBufferStorage overload without data.

--- src/agl/api/opengl/buffer/function/named_buffer_storage.hpp
+++ src/agl/api/opengl/buffer/function/named_buffer_storage.hpp
@@ -2,22 +2,45 @@
 
 #include "buffer.hpp"
 
+#include <initializer_list>
 #include <span>
 
 namespace agl::opengl {
 
-template<typename T>
+inline
 void NamedBufferStorage(
     const Buffer& b,
-    std::span<const T> s,
+    GLsizei size,
     GLenum flags = GL_NONE)
 {
     glNamedBufferStorage(
         b,
-        size(s) * sizeof(T),
-        data(s),
+        size,
+        NULL,
         flags);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Might be better to use a concept encapsulating both init_list and span.
+// Only data, size and value_type are required.
+
+// 'std::initializer_list'.
+
+template<typename T>
+void NamedBufferStorage(
+    const Buffer& b,
+    std::initializer_list<T> il,
+    GLenum flags = GL_NONE)
+{
+    glNamedBufferStorage(
+        b,
+        size(il) * sizeof(T),
+        data(il),
+        flags);
+}
+
+// 'std::span'.
 
 template<typename T>
 void NamedBufferStorage(
@@ -25,7 +48,11 @@ void NamedBufferStorage(
     std::span<T> s,
     GLenum flags = GL_NONE)
 {
-    NamedBufferStorage(b, std::span<const T>(s), flags);
+    glNamedBufferStorage(
+        b,
+        size(s) * sizeof(T),
+        data(s),
+        flags);
 }
 
 }
